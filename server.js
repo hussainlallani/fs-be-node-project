df8d568aa0f22c05a7e80920a0c5df4ea3c60649
Hussain Lallani <hussainlallani@gmail.com>
2025-04-12 04:04:04 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Logger from "./logger.js";
+import { MongoClient } from "mongodb";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,16 +10,31 @@ app.use(express.json());
 
 // Basic route
 app.get("/", (req, res) => {
-  res.send("Welcome to the Resume Builder API!");
+  res.send("Hello World!");
 });
 
-const logger = new Logger();
-// Step1: Register a listener
-logger.on("messageLogged", (arg) => {
-  console.log("Listener called", arg);
-});
+// const logger = new Logger();
+// // Step1: Register a listener
+// logger.on("messageLogged", (arg) => {
+//   console.log("Listener called", arg);
+// });
 
-logger.log("message!!");
+const uri = "mongodb://157.173.198.244:27017"; // or your connection string
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("test");
+    const collection = db.collection("demo");
+    const result = await collection.insertOne({ hello: "world" });
+    console.log(result);
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 // Start the server
 app.listen(PORT, () => {
