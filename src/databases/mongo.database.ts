cc5f7cc4c66908ca273d434b97f4a7dde88611ca
Hussain Lallani <hussainlallani@gmail.com>
2025-06-22 16:10:29 -0400

    changed host

--- src/databases/mongo.database.ts
+++ src/databases/mongo.database.ts
@@ -113,7 +113,7 @@ mongoose.set("strictQuery", false);
 // Replace with your env variable or fallback URI
 const mongoUri =
   process.env.MONGODB_URI ||
-  "mongodb://admin:admin%40123@157.173.198.244:27018/fs_be_node_project?authSource=admin";
+  "mongodb://admin:admin%40123@127.0.0.1:27018/fs_be_node_project?authSource=admin";
 
 // Connect using Mongoose only
 export async function initializeMongoDB(): Promise<typeof mongoose> {
