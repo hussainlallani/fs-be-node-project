ea23570aa431bd01018becf8a77dfc1ff2b71aec
Hussain Lallani <hussainlallani@gmail.com>
2025-09-10 13:42:52 -0400

    testing basepath05

--- gamehub/src/app/page.tsx
+++ gamehub/src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly. DEMO
+            LOCAL Save and see your changes instantly. DEMO
           </li>
         </ol>
 
