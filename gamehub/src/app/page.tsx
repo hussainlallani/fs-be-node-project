eb4c8ff32092d228b7a7309cf9919c9fa28dda27
Hussain Lallani <hussainlallani@gmail.com>
2025-09-10 12:40:31 -0400

    testing next img

--- gamehub/src/app/page.tsx
+++ gamehub/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/gamehub/next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js logo"
           width={180}
           height={38}
@@ -14,7 +16,7 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Get started by editing LOGO{""}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
@@ -34,7 +36,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/gamehub/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -60,7 +62,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/gamehub/file.svg"
             alt="File icon"
             width={16}
             height={16}
