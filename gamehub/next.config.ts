de10166bd3ada6074d1670e8cf4dc4bd7cf466e9
Hussain Lallani <hussainlallani@gmail.com>
2025-09-10 12:29:54 -0400

    updated next config

--- gamehub/next.config.ts
+++ gamehub/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/gamehub",
+  assetPrefix: "/gamehub",
 };
 
 export default nextConfig;
