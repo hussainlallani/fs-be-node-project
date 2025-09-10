a43b9012ce7591b1d0e270228a2aed9bf8190392
Hussain Lallani <hussainlallani@gmail.com>
2025-09-10 13:25:26 -0400

    testing basepath02

--- gamehub/src/app/page.tsx
+++ gamehub/src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 export default function Home() {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
