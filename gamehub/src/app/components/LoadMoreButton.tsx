e3d35f44176014d530ce104dbf1b1222f6ac2ba2
Hussain Lallani <hussainlallani@gmail.com>
2025-09-15 15:43:26 -0400

    added load more button

--- gamehub/src/app/components/LoadMoreButton.tsx
+++ gamehub/src/app/components/LoadMoreButton.tsx
@@ -12,7 +12,7 @@ const LoadMoreButton = ({ onClick, loading, disabled }: Props) => {
       <button
         onClick={onClick}
         disabled={loading || disabled}
-        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50"
+        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 disabled:opacity-50 cursor-pointer"
       >
         {loading ? "Loading..." : "Load More"}
       </button>
