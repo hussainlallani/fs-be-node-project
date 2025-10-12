74aa628569b9826ac047cd8b2f4dffd1d2639f2a
Hussain Lallani <hussainlallani@gmail.com>
2025-10-12 00:19:09 -0400

    added card slideshow

--- gamehub/src/app/components/molecules/SummaryBlock.tsx
+++ gamehub/src/app/components/molecules/SummaryBlock.tsx
@@ -17,12 +17,12 @@ export default function SummaryBlock({
   const isLongSummary = summaryLength > 21;
 
   return (
-    <div className="relative h-10 md:h-14 md:mb-2 border-x border-gray-200 dark:border-gray-700 transition-all duration-300 ease-in-out flex flex-col z-50">
+    <div className="relative h-10 md:h-14 md:mb-2  transition-all duration-300 ease-in-out flex flex-col z-50">
       <div
         className={`text-md text-gray-600 dark:text-gray-300 ${
           cardExpanded
             ? "bg-gray-50 dark:bg-gray-950 break-all px-3 py-2 z-50 rounded-b-lg border-1 border-t-0 border-gray-200 dark:border-gray-700"
-            : "bg-white dark:bg-gray-900 break-all line-clamp-2 px-3 py-2 min-h-14 z-50"
+            : "bg-white dark:bg-gray-900 break-all line-clamp-2 border-x border-gray-200 dark:border-gray-700 px-3 py-2 min-h-14 z-50"
         } ${isLongSummary ? "" : "rounded-b-lg border-b-0"} `}
         title={summary || "No summary available."}
       >
