c255a297f34fe677d864e3e4d561dcd88b9eb198
Hussain Lallani <hussainlallani@gmail.com>
2025-12-11 16:41:09 -0500

    updated next

--- gamehub/next.config.ts
+++ gamehub/next.config.ts
@@ -9,10 +9,24 @@ const nextConfig: NextConfig = {
   env: {
     basePath: "/gamehub",
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/:path*", // apply to all routes
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value:
+  //             "default-src 'none'; connect-src 'self' https://127.0.0.1:3001;",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   images: {
     domains: [
       "images.igdb.com",
-      "media.istockphoto.com", // ✅ add this
+      "media.istockphoto.com", // ✅ added
       "media.rawg.io",
       "www.freetogame.com",
       "flowbite.com",
